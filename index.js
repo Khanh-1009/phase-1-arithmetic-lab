d12fa277fc297f1eb726aabfced4af0d3b374805
Khanh Tran <katetrannbkhanh@gmail.com>
2022-09-29 00:11:50 -0700

    commit tree 316b361acafb47502976a20bc7c34f6e4298c8c4

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 8;
+const num4 = 4;
+const multiply = num1 * num2;
+const random = 2+2;
+const mod = num3 - num4;
+const max = Math.max(2,8,1,20);
